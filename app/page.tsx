bc5ecd25ee2f7bcf409d9a742ee87c84dc335007
Daniel Williams <danwill89@hotmail.com>
2023-11-02 15:10:59 +0000

    fixed sweep issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Card from "./components/card";
 import TitleBar from "./components/title-bar";
 import LeftMenu from "./components/left-menu";
-import styles from "../styles/page.module.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -19,64 +19,4 @@ export default function Home() {
       </div>
     </>
   );
-}
-=======
-import Card from "./components/card";
-import TitleBar from "./components/title-bar";
-import LeftMenu from "./components/left-menu";
-  )
-}
-=======
-  )
-}
-import styles from "../styles/page.module.css";
-
-export default function Home() {
-  return (
-    <>
-      <TitleBar />
-      <div className={styles.pageLayout}>
-        <LeftMenu />
-        <div className={styles.cardContainer}>
-          <Card name="John Doe" age={30} />
-          <Card name="Jane Doe" age={28} />
-          <Card name="Alice Smith" age={35} />
-          <Card name="Bob Johnson" age={40} />
-          <Card name="Charlie Brown" age={50} />
-        </div>
-      </div>
-    </>
-  );
-}
-  )
-}
-        <LeftMenu />
-        <div className={styles.cardContainer}>
-          <Card name="John Doe" age={30} />
-          <Card name="Jane Doe" age={28} />
-          <Card name="Alice Smith" age={35} />
-          <Card name="Bob Johnson" age={40} />
-          <Card name="Charlie Brown" age={50} />
-        </div>
-      </div>
-    </>
-  )
-}
-  )
-}
-)
-    <>
-      <TitleBar />
-      <div className={styles.pageLayout}>
-        <LeftMenu />
-        <div className={styles.cardContainer}>
-          <Card name="John Doe" age={30} />
-          <Card name="Jane Doe" age={28} />
-          <Card name="Alice Smith" age={35} />
-          <Card name="Bob Johnson" age={40} />
-          <Card name="Charlie Brown" age={50} />
-        </div>
-      </div>
-    </>
-  )
 }
